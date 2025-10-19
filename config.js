68a395099ec8b3cda1f3eb0f5de5d3bb115ed4e9
Shivam Kumar <shivamnox0@gmail.com>
2025-10-19 12:59:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,19 @@
-require('dotenv').config(); // Optional: if using a .env file
+require("dotenv").config(); // Optional: if using a .env file
 
 const config = {
-  BOT_TOKEN: process.env.BOT_TOKEN || '7616543161:AAE8jLCI2Ub_vuVRoj6L1fLu8Bl96KwBb9g',
-  MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://daxonultra:fGhF1ggBqt1semxN@cluster0.u1t6q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-  OWNER_ID: process.env.OWNER_ID || '6713397633'
+  BOT_TOKEN:
+    process.env.BOT_TOKEN || "7616543161:AAE8jLCI2Ub_vuVRoj6L1fLu8Bl96KwBb9g",
+  MONGO_URI:
+    process.env.MONGO_URI ||
+    "mongodb+srv://daxonultra:fGhF1ggBqt1semxN@cluster0.tet19g5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  OWNER_ID: process.env.OWNER_ID || 6713397633,
 };
 
 // validation
 if (!config.BOT_TOKEN || !config.MONGO_URI || !config.OWNER_ID) {
-  console.warn('⚠️ Warning: Missing required environment variables in .env file');
+  console.warn(
+    "⚠️ Warning: Missing required environment variables in .env file"
+  );
 }
-
 
 module.exports = config;
