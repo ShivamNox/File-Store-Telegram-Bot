52e5f1237f5868970030add435d93216a2a9a990
Shivam Kumar <shivamnox0@gmail.com>
2025-10-21 13:13:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,10 @@
 require("dotenv").config(); // Optional: if using a .env file
 
 const config = {
-  BOT_TOKEN:
-    process.env.BOT_TOKEN || " ",
-  MONGO_URI:
-    process.env.MONGO_URI ||
-    " ",
-  OWNER_ID: Number(process.env.OWNER_ID || " "),
+  BOT_TOKEN: process.env.BOT_TOKEN || " ",
+  MONGO_URI: process.env.MONGO_URI || " ",
+  OWNER_ID: Number(process.env.OWNER_ID || "6713397633"),
+  START_IMAGE_URL: process.env.START_IMAGE_URL || " ",
 };
 
 // validation
